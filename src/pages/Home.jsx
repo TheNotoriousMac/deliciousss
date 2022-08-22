a7a15ec502e7500acc0f2db1af560fc49f949999
TheNotoriousMac <damani.mccaskie@mycavehill.uwi.edu>
2022-08-21 20:31:52 -0400

    Added Popular and Veggie Components.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Veggie from "../components/Veggie/Veggie";
+import Popular from "../components/Popular/Popular";
+
+
+function Home() {
+  return (
+    <div>
+        <Veggie/> 
+        <Popular/>
+    </div>
+  )
+}
+
+export default Home
